1671a062feae49eb0d9f9f34b5483d7aaed73746
F4RR311 <valery.leo2016@yandex.ru>
2023-02-16 00:46:04 +0300

    webpack config-fix

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,8 +7,9 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
+
     const babelLoader = {
-        test: /\.(js|jsx|ts|tsx)$/,
+        test: /\.(js|jsx|tsx)$/,
         exclude: /node_modules/,
         use: {
             loader: 'babel-loader',
@@ -18,15 +19,39 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                     [
                         'i18next-extract',
                         {
-                            locales: ['en', 'ru'],
-                            keyAsDefaultValue: false,
-                            saveMissing: true,
-                            outputPath: 'public/locales/{{locale}}/{{ns}}.json',
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
                         },
                     ],
                 ],
             },
         },
+    };
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
+            },
+            'sass-loader',
+        ],
+    };
+
+    // Если не используем тайпскрипт - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
     };
 
     const fileLoader = {
@@ -37,40 +62,12 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
             },
         ],
     };
-    const cssLoader = {
 
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]'
-                            : '[hash:base64:8]',
-                    },
-
-                },
-            },
-            'sass-loader'],
-
-    };
-    // Если не используем тайпскрипт, то понадобится бэйбл
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'setupTests.ts-loader',
-        exclude: /node_modules/,
-
-    };
     return [
-        babelLoader,
-
-        cssLoader,
         fileLoader,
-        typescriptLoader,
         svgLoader,
-
+        babelLoader,
+        typescriptLoader,
+        cssLoader,
     ];
 }
