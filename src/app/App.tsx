272f06de97ff04f3e380df96f628f0a4f72458cb
F4RR311 <valery.leo2016@yandex.ru>
2023-02-19 00:01:04 +0300

    fix error build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,8 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
-import { NavBar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
+import { NavBar } from 'widgets/NavBar';
+import { Sidebar } from 'widgets/SideBar';
 
 const App = () => {
     const { theme } = useTheme();
