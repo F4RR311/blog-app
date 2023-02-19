2a0d933ce60ca445337ab0935e99699468021475
F4RR311 <valery.leo2016@yandex.ru>
2023-02-19 19:52:49 +0300

    update sidebar

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './NavBar.module.scss';
 
 interface NavBarProps {
@@ -10,16 +9,7 @@ interface NavBarProps {
 export const NavBar = ({ className }:NavBarProps) => (
     <div className={classNames(cls.Navbar, {}, [className])}>
 
-        <div className={cls.links}>
-            {/* eslint-disable-next-line i18next/no-literal-string */}
-            <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to="/">
-                Главная страница
-            </AppLink>
-            {/* eslint-disable-next-line i18next/no-literal-string */}
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-                О сайте
-            </AppLink>
-        </div>
+        <div className={cls.links} />
 
     </div>
 );
