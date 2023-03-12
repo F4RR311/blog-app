0cb0a47bd7a99689ef284ccc8662312894bb0de5
F4RR311 <valery.leo2016@yandex.ru>
2023-03-12 23:28:59 +0300

    configuration

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,10 +1,9 @@
 // import React from 'react';
 // import { ComponentMeta, ComponentStory } from '@storybook/react';
-// import 'app/styles/index.scss';
 // import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 // import { Theme } from 'app/providers/ThemeProvider';
+// import ProfilePage from 'pages/ProfilePage/ui/ProfilePage';
 // import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
-// import ProfilePage from './ProfilePage';
 //
 // export default {
 //     title: 'pages/ProfilePage',
@@ -12,26 +11,14 @@
 //     argTypes: {
 //         backgroundColor: { control: 'color' },
 //     },
-//     args: {
-//         to: '/',
-//     },
 // } as ComponentMeta<typeof ProfilePage>;
-// // @ts-ignore
+//
 // const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage {...args} />;
 //
 // export const Normal = Template.bind({});
-//
-// Normal.args = {
-//
-// };
-//
-// // @ts-ignore
-// Normal.decorators = [StoreDecorator({ })];
+// Normal.args = {};
+// // Normal.decorators = [StoreDecorator({})];
 //
 // export const Dark = Template.bind({});
-// Dark.args = {
-//
-// };
-//
-// // @ts-ignore
-// Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({ })];
+// Dark.args = {};
+// // Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
