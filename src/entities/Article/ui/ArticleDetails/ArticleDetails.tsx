a9599e8797e6104071c8908a41772173ec9632e9
F4RR311 <valery.leo2016@yandex.ru>
2023-03-19 00:36:23 +0300

    skeleton dev-op

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -4,25 +4,78 @@ import {
     DynamicModuleLoader,
     ReducersList,
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { memo, useEffect } from 'react';
+import {
+    fetchArticleById,
+} from 'entities/Article/model/services/fetchArticleById/fetchArticleById';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { Text, TextAlign } from 'shared/ui/Text/Text';
+import { Skeleton } from 'shared/ui/skeleton/Skeleton';
+import {
+    getArticleDetailsData,
+    getArticleDetailsError,
+    getArticleDetailsIsLoading,
+} from '../../model/selectors/articleDetails';
 import cls from './ArticleDetails.module.scss';
 import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
 
 interface ArticleDetailsProps {
     className?: string;
+    id:string
 }
 
 const reducers: ReducersList = {
     articleDetails: articleDetailsReducer,
 };
 
-export const ArticleDetails = ({ className }: ArticleDetailsProps) => {
-    const { t } = useTranslation();
+export const ArticleDetails = memo((props: ArticleDetailsProps) => {
+    const {
+        className,
+        id,
+    } = props;
+    const { t } = useTranslation('article-details');
+    const dispatch = useAppDispatch();
+    // const isLoading = useSelector(getArticleDetailsIsLoading);
+    const isLoading = true;
+    const article = useSelector(getArticleDetailsData);
+    const error = useSelector(getArticleDetailsError);
+    useEffect(() => {
+        // @ts-ignore
+        // TODO Разобраться с типамu
+        dispatch(fetchArticleById(id));
+    }, [dispatch, id]);
+    let content;
+
+    if (isLoading) {
+        content = (
+            <div>
+                <Skeleton className={cls.avatar} width={200} height={200} border="50%" />
+                <Skeleton className={cls.title} width={300} height={32} />
+                <Skeleton className={cls.skeleton} width={600} height={24} />
+                <Skeleton className={cls.skeleton} width="100%" height={200} />
+                <Skeleton className={cls.skeleton} width="100%" height={200} />
+
+            </div>
+        );
+    } else if (error) {
+        content = (
+            <Text
+                align={TextAlign.CENTER}
+                title={t('Произошла ошибка при загрузке статьи')}
+            />
+        );
+    } else {
+        content = (
+            <div>ARTCLE DETAILS</div>
+        );
+    }
+
     return (
         <div className={classNames(cls.ArticleDetails, {}, [className])}>
             <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-
-                ARticle
+                {content}
             </DynamicModuleLoader>
         </div>
     );
-};
+});
