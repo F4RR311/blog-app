a0b95a2b01d3e97ebe048cde8cab27f50395dcfa
F4RR311 <valery.leo2016@yandex.ru>
2023-04-10 00:39:37 +0300

    flex-component

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
         '@typescript-eslint',
         'i18next',
         'react-hooks',
+        'f4rr311-plugin',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -33,7 +34,7 @@ module.exports = {
         ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
-        'no-unused-vars': 'warn',
+        'no-unused-vars': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
@@ -46,30 +47,31 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to'],
+                ignoreAttribute: ['data-testid', 'to', 'target'],
             },
         ],
-        'max-len': ['error', { ignoreComments: true, code: 150 }],
-        'jsx-a11y.no-static-element-interactions': 'off',
+        'max-len': ['error', { ignoreComments: true, code: 125 }],
+        'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
+        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
         'no-param-reassign': 'off',
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-
+        'f4rr311-plugin/path-checker': 'error',
     },
     globals: {
         __IS_DEV__: true,
         __API__: true,
-        __PROJECT: true,
+        __PROJECT__: true,
     },
     overrides: [
         {
-            files: ['**/src/**/*.test.{ts,tsx}'],
+            files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
             rules: {
                 'i18next/no-literal-string': 'off',
+                'max-len': 'off',
             },
         },
     ],
