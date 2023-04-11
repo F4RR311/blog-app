19d392726c7eee7143d237b63fbde77a33d1e618
F4RR311 <valery.leo2016@yandex.ru>
2023-04-12 01:50:39 +0300

    some semantic

--- src/shared/ui/Text/Text.stories.tsx
+++ src/shared/ui/Text/Text.stories.tsx
@@ -67,3 +67,19 @@ SizeL.args = {
     size: TextSize.L,
 
 };
+
+export const SizeM = Template.bind({});
+SizeM.args = {
+    title: 'Title lorem ipsun',
+    text: 'Description Description Description Description',
+    size: TextSize.M,
+
+};
+
+export const SizeS = Template.bind({});
+SizeS.args = {
+    title: 'Title lorem ipsun',
+    text: 'Description Description Description Description',
+    size: TextSize.S,
+
+};
