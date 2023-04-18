6e0f5d77926884447a342122e6b7e2a7fa405bd2
F4RR311 <valery.leo2016@yandex.ru>
2023-04-19 00:30:26 +0300

    fix styles bugs

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -22,13 +22,13 @@ export const CommentCard = (props: CommentCardProps) => {
     } = props;
     if (isLoading) {
         return (
-            <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+            <VStack gap="16" max className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
                 <div className={cls.header}>
                     <Skeleton width={30} height={30} border="50%" />
                     <Skeleton width={100} height={16} className={cls.username} />
                 </div>
                 <Skeleton width="100" height={50} className={cls.text} />
-            </div>
+            </VStack>
         );
     }
 
