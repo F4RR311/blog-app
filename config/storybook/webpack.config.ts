16cea20f1d78c7adc3589a568795a11e371ca3a1
F4RR311 <valery.leo2016@yandex.ru>
2023-04-30 21:28:03 +0300

    some fix storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -14,6 +14,11 @@ export default ({ config }: {config: webpack.Configuration}) => {
     };
     config!.resolve!.modules!.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+
+    };
 
     /* исправление ошибки storybook */
     if (config!.resolve!.modules) {
