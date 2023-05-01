5b7df675b340013aa6b2da5bfee67b8188a676ae
F4RR311 <valery.leo2016@yandex.ru>
2023-05-02 00:04:17 +0300

    fix import bug!

--- src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { StoreDecorator } from '@shared/config/storybook/StoreDecorator/StoreDecorator';
+
 import ArticleRating from './ArticleRating';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
     title: 'features/ArticleRating',
