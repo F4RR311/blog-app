4ff9a1301917e9ff8eadf276485cc662ef4f4bfb
F4RR311 <valery.leo2016@yandex.ru>
2023-05-13 09:59:57 +0300

    add theme addon storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,6 +3,7 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import { Theme } from '../../src/shared/const/theme';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,6 +12,15 @@ export const parameters = {
             color: /(background|color)$/i,
             date: /Date$/,
         },
+    },
+    layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#00aced' },
+            { name: 'dark', class: Theme.DARK, color: '#15306b' },
+            { name: 'pink', class: Theme.PINK, color: '#cd3bee' },
+        ],
     },
 };
 
