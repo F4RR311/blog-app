35cff5d83e29fab5e619360c045b3b12206ff037
F4RR311 <valery.leo2016@yandex.ru>
2023-05-17 23:38:42 +0300

    refactor routing

--- src/shared/const/router.ts
+++ src/shared/const/router.ts
@@ -22,5 +22,6 @@ export const getRouteArticleCreate = () => '/articles/new';
 export const getRouteArticleEdit = (id:string) => `/articles/${id}/edit`;
 export const getRouteAdmin = () => '/admin';
 export const getRouteForbidden = () => '/forbidden';
+export const getRoutePopover = () => '/popover';
 
 // TODO Articles EDit an Preview fix it!
