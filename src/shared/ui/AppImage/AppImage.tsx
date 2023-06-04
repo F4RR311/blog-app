9a798e31a10e6308aa4ac0c7ce82543140e2ce4c
F4RR311 <valery.leo2016@yandex.ru>
2023-06-04 23:54:21 +0300

    img preload

---
+++ src/shared/ui/AppImage/AppImage.tsx
@@ -0,0 +1,36 @@
+import {
+    ImgHTMLAttributes, ReactElement, useLayoutEffect, useState,
+} from 'react';
+
+interface AppImageProps extends ImgHTMLAttributes<HTMLElement>{
+    className?: string;
+    fallback?:ReactElement;
+    errorFallback?: ReactElement;
+}
+
+export const AppImage = (props: AppImageProps) => {
+    const {
+        className, src, alt = '', fallback, errorFallback, ...otherProps
+    } = props;
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
+    useLayoutEffect(() => {
+        const img = new Image();
+        img.src = src ?? '';
+        img.onload = () => {
+            setIsLoading(false);
+        };
+    }, [src]);
+
+    if (isLoading && fallback) {
+        return fallback;
+    }
+
+    if (hasError && errorFallback) {
+        return errorFallback;
+    }
+    return (
+        <img className={className} src={src} alt={alt} {...otherProps} />
+
+    );
+};
